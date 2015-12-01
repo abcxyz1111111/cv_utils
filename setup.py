01f3859c7f338d6bcdd18377d7a02880e12c58b9
Daniel Nugent <djnugent@iastate.edu>
2015-12-01 14:53:53 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy','pymavlink','pyserial','python-opencv'],
+    install_requires=['numpy','pymavlink','pyserial'],
 
 )
